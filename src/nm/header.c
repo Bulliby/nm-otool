8ebdc943bb48b6b7a707f1084346b9acf1f78470
Bulliby <gwells@student.42.fr>
2016-12-29 16:08:38 +0100

    feat(master): correct of segfault on dylib and ar

--- src/nm/header.c
+++ src/nm/header.c
@@ -11,12 +11,18 @@
 /* ************************************************************************** */
 
 #include "nmotool.h"
+#include <ar.h>
 
 void			header64(const t_header64 *header)
 {
 	uint32_t	ncmds;
 	t_lc		*lc;
 
+	if (!ft_memcmp(header, ARMAG, SARMAG) || header->cputype != CPU_TYPE_X86_64)
+	{
+		ft_putendl_fd("Bad Architecture", 2);
+		return ;
+	}
 	ncmds = header->ncmds;
 	lc = (void *)header + sizeof(*header);
 	parse64nm(header, ncmds, lc);
@@ -27,6 +33,11 @@ void			header32(const t_header32 *header)
 	uint32_t	ncmds;
 	t_lc		*lc;
 
+	if (!ft_memcmp(header, ARMAG, SARMAG) || header->cputype != CPU_TYPE_X86)
+	{
+		ft_putendl_fd("Bad Architecture", 2);
+		return ;
+	}
 	ncmds = header->ncmds;
 	lc = (void *)header + sizeof(*header);
 	parse32nm(header, ncmds, lc);
--- src/otool/header.c
+++ src/otool/header.c
@@ -11,12 +11,18 @@
 /* ************************************************************************** */
 
 #include "nmotool.h"
+#include <ar.h>
 
 void			header64(const t_header64 *header)
 {
 	uint32_t	ncmds;
 	t_seg64		*seg;
 
+	if (!ft_memcmp(header, ARMAG, SARMAG) || header->cputype != CPU_TYPE_X86_64)
+	{
+		ft_putendl_fd("Bad Architecture", 2);
+		return ;
+	}
 	ncmds = header->ncmds;
 	seg = (void *)header + sizeof(*header);
 	parse64(header, ncmds, seg);
@@ -27,6 +33,11 @@ void			header32(const t_header32 *header)
 	uint32_t	ncmds;
 	t_seg32		*seg;
 
+	if (!ft_memcmp(header, ARMAG, SARMAG) || header->cputype != CPU_TYPE_X86_64)
+	{
+		ft_putendl_fd("Bad Architecture", 2);
+		return ;
+	}
 	ncmds = header->ncmds;
 	seg = (void *)header + sizeof(*header);
 	parse32(header, ncmds, seg);
